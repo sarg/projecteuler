60c4ce6e35c7a2f5fcdd84c93a4de9799fc1dcf1
Sergey Trofimov <sarg@sarg.org.ru>
2016-10-17 23:15:40 +0300

    358 solved

--- 358.py
+++ 358.py
@@ -29,36 +29,35 @@ def find_rem(k, x, rem):
 
     return ret
 
-print [ find_rem(5, 56789, rem) for rem in (1,13,137,90000,67890, 78900, 89000) ]
-print find_rem(4,8789,5678)
-lst = find_rem(5, 56789, 10**5-1)
-print lst
+# print [ find_rem(5, 56789, rem) for rem in (1,13,137,90000,67890, 78900, 89000) ]
+# print find_rem(4,8789,5678)
+# lst = find_rem(5, 56789, 10**5-1)
+# print lst
 
-sums = [ digit_sum(a) for a in range(0,10**5) ]
-total = sum(sums[56789 * a % 10**5] for a in range(1,9891+10**5))
-print total/5
+n = 729809890
+# more 
 
-# n = 100009891
-# for i in range(1,10**5):
-#     rem = 56789 * i % 100000
-#     cnt = ((n-rem) / 10**5)
-#     if cnt < 1:
-#         print i
-        
-#     total += sums[rem] * cnt
-# print total
-
-
-i = 9891
-while True:
-    i += 10**5
-    if 10**
+last_digit = 9
+total = 0
+for rem in xrange(0,10):
+    i = find_rem(1, last_digit, rem)
+    if i > n:
+        continue
+    cnt = int(((n-i) / 10)) + 1
+    total += rem * cnt
+print total
 
 
 """
 maximal result
 00000000137...56789 x ...2902 =
 900000000137...5678
+
+00000000137...56789 x ... =
+99999000000...
+
+minimal cycle is greater than
+90000000013/138 = 652173913
 
 cycle gives number of digits
 00000000137...56789 x ...9891 =
@@ -67,4 +66,12 @@ cycle gives number of digits
 (10**n - 1) mod (n+1) = 0
 or 10**n mod (n+1) = 1
 where n=...9890
+
+because there is minimal cycle then 9891 is not it
+and in fact the cycle ends with 09891
+
+let's check
+652109891
+
+for i: 724609890 thru 730000000 step 10^5 do if power_mod(10,i,i+1)=1 then print(i);
 """
